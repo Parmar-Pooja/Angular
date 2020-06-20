3a7473a0df1693c998670da67e108d82249528b9
Parmar-Pooja <parmarpooja1352@gmail.com>
2020-06-20 15:28:56 +0530

    HTTP PART-1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit , ViewChild, Inject } from '@angular/core';
+import { Component, OnInit ,Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
+import { switchMap } from 'rxjs/operators';
 import { DishService } from '../services/dish.service';
+import {FormBuilder , FormGroup , Validators} from '@angular/forms';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { switchMap } from 'rxjs/operators';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Comment } from '../shared/comment';
+import {Comment} from '../shared/comment';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,79 +16,91 @@ import { Comment } from '../shared/comment';
 
 export class DishdetailComponent implements OnInit {
 
-  commentForm: FormGroup;
-  comment: Comment;
- 
-  formErrors = {
-    'author': '',
-    'rating': 5,
-    'comment': ''
-  };
-
-  validationMessages = {
-    'author': {
-      'required': 'Author is required',
-      'minlength': 'Author must be 2 characters long',
-      'maxlength': 'Author can not be more than 25'
-    },
-    'comment': {
-      'required': 'Comment is required',
-      'minlength': 'Comment must be 2 characters long'
-    }
-  }
-
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  commentForm: FormGroup;
+  comment: Comment;
+  formErrors = {
+    author: '' ,
+    rating: 5 ,
+    comment: ''
+  };
+  validationMessages = {
+    'author': {
+      'required': 'Author is required.' ,
+      'minlength': 'Author must be at least 2 characters long.'
+    } ,
+    'comment': {
+      'required': 'comment is required.' ,
+      'minlength': 'comment must be at least 2 characters long.'
+    } ,
+  };
 
-  constructor(private dishservice: DishService,
-    private route: ActivatedRoute,
-    private location: Location,
-    private fb: FormBuilder) {
-      this.createForm();
-     }
+  constructor(private dishservice: DishService ,
+              private route: ActivatedRoute , 
+              private location: Location ,
+              private fb: FormBuilder,
+              @Inject('BaseURL') public BaseURL) {
+    this.createForm();
+  }
+
 
   ngOnInit() {
-    this.dishservice.getDishIds()
-      .subscribe(dishIds => {this.dishIds= dishIds;});
+    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-      .subscribe(dish => {this.dish = dish; this.setPrevNext(dish.id) } );
+    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
   }
 
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
-    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length]
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
     this.location.back();
   }
 
-private  createForm() {
+  private createForm(): void {
     this.commentForm = this.fb.group({
-      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      rating: 5,
-      comment: ['',[ Validators.required, Validators.minLength(2)] ]
+      author: ['' , [Validators.required , Validators.minLength(2)]] ,
+      rating: 5 ,
+      comment: ['' , [Validators.required , Validators.minLength(2)]]
     });
 
-    this.commentForm.valueChanges.subscribe(data => this.onValueChanged(data));
-    this.onValueChanged(); //(re)set form validation messages
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); 
+  }
+
+  onSubmit() {
+    this.comment = this.commentForm.value;
+    this.comment.date = new Date().toISOString();
+    this.dish.comments.push(this.comment);
+    this.commentForm.reset({
+      author: '' ,
+      rating: 5 ,
+      comment: ''
+    });
   }
 
   onValueChanged(data?: any) {
-    if(!this.commentForm) { return; }
+    if (!this.commentForm) {
+      return;
+    }
     const form = this.commentForm;
-    for(const field in this.formErrors) {
-      if(this.formErrors.hasOwnProperty(field)){
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
         // clear previous error message (if any)
         this.formErrors[field] = '';
         const control = form.get(field);
-        if(control && control.dirty && !control.valid) {
+        if (control && control.dirty && !control.valid) {
           const messages = this.validationMessages[field];
-          for (const key in control.errors ) {
-            if ( control.errors.hasOwnProperty(key)) {
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
               this.formErrors[field] += messages[key] + ' ';
             }
           }
@@ -97,16 +109,5 @@ private  createForm() {
     }
   }
 
-  onSubmit() {
-    this.comment = this.commentForm.value;
-    this.comment.date = new Date().toISOString();
-    this.dish.comments.push(this.comment);
-    console.log(this.comment);
-    this.commentForm.reset({
-      author: '',
-      rating: 5,
-      comment: ''
-    });
-  }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 import { Promotion } from '../shared/promotion';
@@ -12,21 +12,21 @@ import { LeaderService } from '../services/leader.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  
-  leader: Leader;
+
   dish: Dish;
   promotion: Promotion;
+  leader: Leader;
 
-  constructor(private dishService: DishService,
-    private promotionService: PromotionService,
-    private leaderService: LeaderService) { }
+  constructor(private dishservice: DishService,
+    private leaderservice: LeaderService,
+    private promotionservice: PromotionService,
+    @Inject('BaseURL') public BaseURL) { }
 
-  ngOnInit(): void {
-    this.dishService.getFeaturedDish()
-      .subscribe(dish => this.dish = dish);
-    this.promotionService.getFeaturedPromotion()
-      .subscribe(promotion => this.promotion = promotion);
-    this.leaderService.getFeaturedLeader()
-      .subscribe(leader => this.leader = leader);
+  ngOnInit() {
+    this.dishservice.getFeaturedDish().subscribe(dish => this.dish = dish);
+    this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
+    this.leaderservice.getFeaturedLeader().subscribe(leader => this.leader = leader);
+    
   }
+
 }
