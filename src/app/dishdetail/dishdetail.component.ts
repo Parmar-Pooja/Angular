197240e2465c6db7d9cf49d0de4b5e2c56352999
Parmar-Pooja <parmarpooja1352@gmail.com>
2020-06-20 17:13:41 +0530

    Saving Changes To Server

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -21,6 +21,7 @@ export class DishdetailComponent implements OnInit {
   dishIds: string[];
   prev: string;
   next: string;
+  dishcopy: Dish;
   commentForm: FormGroup;
   comment: Comment;
   formErrors = {
@@ -51,7 +52,7 @@ export class DishdetailComponent implements OnInit {
   ngOnInit() {
     this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
     this.route.params.pipe(switchMap((params: Params) => this.dishservice.getDish(params['id'])))
-    .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); }, 
+    .subscribe(dish => { this.dish = dish; this.dishcopy= dish; this.setPrevNext(dish.id); }, 
     errmess => this.errMess = <any> errmess);
   }
 
@@ -81,7 +82,13 @@ export class DishdetailComponent implements OnInit {
   onSubmit() {
     this.comment = this.commentForm.value;
     this.comment.date = new Date().toISOString();
-    this.dish.comments.push(this.comment);
+    console.log(this.comment);
+    this.dishcopy.comments.push(this.comment);
+    this,this.dishservice.putDish(this.dishcopy)
+      .subscribe(dish => {
+        this.dish=dish; this.dishcopy=dish;
+      },
+      errmess => {this.dish= null; this.dishcopy=null; this.errMess= <any>errmess});
     this.commentForm.reset({
       author: '' ,
       rating: 5 ,
